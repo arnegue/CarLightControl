943384d7d39f83827f77a020dbfcc61c906dc312
Arne <arne-@hotmail.de>
2024-03-27 13:12:12 +0100

    #3 OTA Update now working

--- src/carlightcontrol.cpp
+++ src/carlightcontrol.cpp
@@ -1,10 +1,11 @@
 #include <ESPAsyncWebServer.h>
+#include <Update.h>
 
 #include "wifi_secrets.h"
 #include "pwm_switch.h"
 #include "blinking_pwm_switch.h"
 
-#define ENABLE_POTENTIOMETER /* You may want to disable them for testing */
+#define ENABLE_POTENTIOMETER /* You might want to disable them for testing */
 
 AsyncWebServer server(80);
 
@@ -13,13 +14,15 @@ const char index_html[] PROGMEM =
 #include "index.h" // unfortunatelly this only works when manipulating the html to a raw-string, and changing the file extension
     ;
 
+// clang-format off
 // Create instances of Lights
 //                         Name             PWMPin, PotiPin
 PWMSwitch ruecklicht{     "Ruecklicht",     13,     35};
 PWMSwitch bremslicht{     "BremsLicht",     27,     34};
 BlinkingPWMSwitch blinker{"Blinker",        33,     39};
 PWMSwitch rueckfahrlicht{ "Rueckfahrlicht", 32,     36};
-    
+// clang-format on
+
 // Add lights to vector
 const static std::vector<PWMSwitch *> switches = {
     &ruecklicht,
@@ -53,6 +56,10 @@ String processor(const String &var)
             ret_str += "</tr>";
         }
     }
+    else if (var == "DATE")
+    {
+        ret_str = __DATE__ " " __TIME__;
+    }
     return ret_str;
 }
 
@@ -73,7 +80,7 @@ const char *PARAM_INPUT_1 = "name";
 const char *PARAM_INPUT_2 = "state";
 const char *PARAM_INPUT_3 = "value";
 
-// Setup routine. Setups Serial, WiFi, Switches and instatiates callback for HTTP_Server
+// Setup routine. Setups Serial, WiFi, Switches and instantiates callback for HTTP_Server
 void setup()
 {
     Serial.begin(115200);
@@ -136,12 +143,69 @@ void setup()
         }
         request->send(200, "text/plain", "OK"); });
 
+    // Handle firmware update  (OTA)
+    server.on(
+        "/upload", HTTP_POST, [&](AsyncWebServerRequest *request)
+        {
+        // the request handler is triggered after the upload has finished... 
+        // create the response, add header, and send response
+        AsyncWebServerResponse *response = request->beginResponse((Update.hasError())?500:200, "text/plain", (Update.hasError())?"FAIL":"OK");
+        response->addHeader("Connection", "close");
+        response->addHeader("Access-Control-Allow-Origin", "*");
+        request->send(response);
+        delay(200);
+        ESP.restart(); },
+        [&](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final)
+        {
+            // Upload handler chunks in data
+            if (!index)
+            {
+                Serial.println("Starting firmware update");
+                // Switch off all lamps
+                for (auto pwm_switch : switches)
+                {
+                    pwm_switch->setValue(0);
+                }
+
+                int cmd = (filename == "filesystem") ? U_SPIFFS : U_FLASH;
+                if (!Update.begin(UPDATE_SIZE_UNKNOWN, cmd))
+                { // Start with max available size
+                    Update.printError(Serial);
+                    return request->send(400, "text/plain", "OTA could not begin");
+                }
+            }
+
+            // Write chunked data to the free sketch space
+            if (len)
+            {
+                if (Update.write(data, len) != len)
+                {
+                    return request->send(400, "text/plain", "OTA could not begin");
+                }
+            }
+
+            // if the final flag is set then this is the last frame of data
+            if (final)
+            {
+                Serial.println("Finished update");
+                if (!Update.end(true))
+                { // true to set the size to the current progress
+                    Update.printError(Serial);
+                    return request->send(400, "text/plain", "Could not end OTA");
+                }
+            }
+            else
+            {
+                return;
+            }
+        });
+
     // Set callback if request is unknown
     server.onNotFound([](AsyncWebServerRequest const *request)
                       {
-    String unkown_request = request->url();
-    Serial.print("Unknown request: ");
-    Serial.println(unkown_request); });
+        String unkown_request = request->url();
+        Serial.print("Unknown request: ");
+        Serial.println(unkown_request); });
 
     // Start server
     server.begin();
@@ -156,5 +220,5 @@ void loop()
         pwm_switch->measure_potentiometer_set_value();
     }
 #endif
-    delay(500);
+    delay(100);
 }
--- src/index.h
+++ src/index.h
@@ -4,9 +4,9 @@ R"rawliteral(
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Headlight control</title>
+    <title>Taillight control</title>
     <link href="https://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
-	<link rel="icon" href="data:,">
+    <link rel="icon" href="data:,">
     <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
 </head>
 <body>
@@ -18,12 +18,23 @@ R"rawliteral(
                 <th>On/Off</th>
                 <th>Value</th>
             </tr>
-			%BUTTON_REPLACE%
+            %BUTTON_REPLACE%
         </table>
     </div>
-</div>
 
+    <form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>
+    <input type='file' name='update' accept=".bin" id='file'>
+    <input type='submit' class=btn value='Update'>
+    <br>
+        <div id='prgbar'>
+            <div id='bar'></div>
+        </div>
+    <br>
+    <p>Compiled on %DATE%</p>
+</form>
+</div>
 <script type="text/javascript">
+    // Light control block (update)
     $("input" ).on( "change", function( event ) {
         var value = null
         var name = event.target.id
@@ -31,24 +42,58 @@ R"rawliteral(
         if (event.target.type === "checkbox") {
             console.log("Checkbox")  // value is "on". better take checked
             value = this.checked
-			url += "state=" + Number(value)
+            url += "state=" + Number(value)
         } else if (event.target.type === "range") {
             console.log("Slider")
             value = this.value
-			url += "value=" + Number(value)
+            url += "value=" + Number(value)
         } else {
             console.log("Unknown source called from " + event.target.id + " with value " + this.value)
             return;
         }
         console.log(event.target.type + " called from " + name + " with value " + value)
         const Http = new XMLHttpRequest();
-        //const url='./update?name=' + name + "&value;
         console.log(url);
         Http.open("GET", url);
         Http.send();
         Http.onreadystatechange=(e)=>{
             console.log(Http.responseText)
         }
+    });
+
+    // Updater block (upload)
+    $('form').submit(function(e){
+        console.log("Starting update");
+        e.preventDefault();
+        var form = $('#upload_form')[0];
+        var data = new FormData(form);
+        $.ajax({
+        url: '/upload',
+        type: 'POST',
+        data: data,
+        contentType: false,
+        processData:false,
+        xhr: function() {
+            var xhr = new window.XMLHttpRequest();
+            xhr.upload.addEventListener('progress', function(evt) {
+                if (evt.lengthComputable) {
+                    var per = evt.loaded / evt.total;
+                    console.log(per);
+                    $('#prg').html('progress: ' + Math.round(per*100) + '%');
+                    $('#bar').css('width',Math.round(per*100) + '%');
+                }
+            }, false);
+            return xhr;
+        },
+        success:function(d, s) {
+            console.log('success!'
+            location.reload();    
+        },
+            error: function (a, b, c) {
+            console.log('error!')    
+            location.reload();    
+        }
+        });
     });
 </script>
 </body>
